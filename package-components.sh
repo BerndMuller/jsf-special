8d25924ff7f9374a9faf1ef7620650d5f12c84e6
Bernd Mueller <bernd.mueller@ostfalia.de>
2020-01-05 19:07:24 +0100

    Native Component Resources

--- package-components.sh
+++ package-components.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # erzeugt JAR (target/components.jar) für native Komponenten
+mkdir -p target/META-INF/resources/css
+mkdir -p target/META-INF/resources/images
+mkdir -p target/META-INF/resources/js
+cp src/main/webapp/resources/st.css target/META-INF/resources/css/expanding-list.css
+cp src/main/webapp/resources/js/expanding-list.js target/META-INF/resources/js/expanding-list.js
+cp src/main/webapp/resources/images/down.svg target/META-INF/resources/images/down.svg
+cp src/main/webapp/resources/images/right.svg target/META-INF/resources/images/right.svg
 jar cvf target/components.jar \
     -C target/classes/ de/jsfpraxis/special/components/Greeting.class \
-    -C target/classes/ de/jsfpraxis/special/components/HelloWorld.class
-
+    -C target/classes/ de/jsfpraxis/special/components/HelloWorld.class \
+    -C target/classes/ de/jsfpraxis/special/components/Tree.class \
+    -C target META-INF
